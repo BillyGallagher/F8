b97dfa7ecc438658dcbf747194805efd18bde478
Billy Gallagher <odd822@gmail.com>
2021-05-16 17:52:43 -0500

    Renown

--- F8/F8.cpp
+++ F8/F8.cpp
@@ -1,4 +1,38 @@
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <vector>
+#include <Windows.h>
+#include "proc.h"
+
 int main()
 {
+	DWORD procId = GetProcId(L"fate.exe");
+	uintptr_t moduleBase = GetModuleBaseAddress(procId, L"fate.exe");
+
+	HANDLE hProcess = 0;
+	hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
+
+	uintptr_t dynamicPtrBaseAddr = moduleBase + 0x748900; 
+
+	std::cout << "DynamicPtrBaseAddr = " << "0x" << std::hex << dynamicPtrBaseAddr << std::endl;
+
+	// Resolve renown pointer chain
+	std::vector<unsigned int> renownLvlOffsets = { 0x80, 0x390 };
+	uintptr_t renownLvlAddr = FindDMAAddress(hProcess, dynamicPtrBaseAddr, renownLvlOffsets);
+
+	std::cout << "renownLvlAddr = " << "0x" << std::hex << renownLvlAddr << std::endl;
+
+	int renownLvlValue = 0;
+	ReadProcessMemory(hProcess, (BYTE*)renownLvlAddr, &renownLvlValue, sizeof(renownLvlValue), nullptr);
+	std::cout << "Current renownLvl = " << std::dec << renownLvlValue << std::endl;
+
+	int newRenownLvl = 10;
+	WriteProcessMemory(hProcess, (BYTE*)renownLvlAddr, &newRenownLvl, sizeof(newRenownLvl), nullptr);
+
+	ReadProcessMemory(hProcess, (BYTE*)renownLvlAddr, &renownLvlValue, sizeof(renownLvlValue), nullptr);
+	std::cout << "New renownLvl = " << std::dec << renownLvlValue << std::endl;
+
+	getchar();
 	return 0;
 }
---
+++ F8/proc.cpp
@@ -0,0 +1,61 @@
+#include "proc.h"
+
+DWORD GetProcId(const wchar_t* procName)
+{
+	DWORD procId = 0;
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+	if (hSnap != INVALID_HANDLE_VALUE) {
+		PROCESSENTRY32 procEntry;
+		procEntry.dwSize = sizeof(procEntry);
+
+		if (Process32First(hSnap, &procEntry))
+		{
+			do
+			{
+				if (!_wcsicmp(procEntry.szExeFile, procName)) {
+					procId = procEntry.th32ProcessID;
+					break;
+				}
+			} while (Process32Next(hSnap, &procEntry));
+		}
+	}
+
+	CloseHandle(hSnap);
+	return procId;
+}
+
+uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* moduleName)
+{
+	uintptr_t moduleBaseAddress = 0;
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
+	if (hSnap != INVALID_HANDLE_VALUE) {
+		MODULEENTRY32 moduleEntry;
+		moduleEntry.dwSize = sizeof(moduleEntry);
+
+		if (Module32First(hSnap, &moduleEntry)) {
+			do 
+			{
+				if (!_wcsicmp(moduleEntry.szModule, moduleName))
+				{
+					moduleBaseAddress = (uintptr_t)moduleEntry.modBaseAddr;
+					break;
+				}
+			} while (Module32Next(hSnap, &moduleEntry));
+		}
+	}
+	
+	CloseHandle(hSnap);
+	return moduleBaseAddress;
+}
+
+uintptr_t FindDMAAddress(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets)
+{
+	uintptr_t addr = ptr;
+	for (unsigned int i = 0; i < offsets.size(); ++i)
+	{
+		ReadProcessMemory(hProc, (BYTE*)addr, &addr, sizeof(addr), 0);
+		addr += offsets[i];
+	}
+	return addr;
+}
